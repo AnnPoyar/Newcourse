1ccdbc54faddd5482a955956c1f875d029b55d84
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-08-06 14:56:21 +0300

    Arrey

---
+++ Ex012MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {16, 25, 41, 6, 72, 8, 35, 41};
+int n = array.Length;
+int find = 41;
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
